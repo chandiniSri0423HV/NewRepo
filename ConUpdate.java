58af54a921e475e385e30eeeee291e644998d717
Chandinisri0423HV <Chandini.srisailaxmiboddu@hitachivantara.com>
2023-07-12 23:08:35 +0530

    .

--- ConUpdate.java
+++ ConUpdate.java
@@ -2,10 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import org.json.simple.JSONObject;
 
 public class ConUpdate {
@@ -17,6 +14,10 @@ public class ConUpdate {
 
         String csvFilePath = args[0];
 
+        // Read CSV file and process the data
+        List<JSONObject> connectionProperties = new ArrayList<>();
+        List<JSONObject> securityProperties = new ArrayList<>();
+
         try (BufferedReader br = new BufferedReader(new FileReader(csvFilePath))) {
             String headerLine = br.readLine();
             if (headerLine == null) {
@@ -26,38 +27,27 @@ public class ConUpdate {
 
             String[] header = headerLine.split(",");
 
-            List<JSONObject> connectionProperties = new ArrayList<>();
-            List<JSONObject> securityProperties = new ArrayList<>();
-
             String line;
-            int row = 1;
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(",", -1); // Split the line and include empty values
 
-               // Create the connection property
-JSONObject connectionProperty = createConnectionProperty();
-connectionProperties.add(connectionProperty);
+                JSONObject connectionProperty = createConnectionProperty(header, values);
+                connectionProperties.add(connectionProperty);
 
-// Create the security properties
-JSONObject usernameProperty = createSecurityProperty("Username", "devops_user");
-securityProperties.add(usernameProperty);
+                JSONObject securityProperty = createSecurityProperty(header, values);
+                securityProperties.add(securityProperty);
 
-JSONObject passwordProperty = createSecurityProperty("Password", "Oic_Jenkins#2023");
-securityProperties.add(passwordProperty);
-
-// Execute the curl command
-executeCurlCommand(connectionProperty, securityProperties);
-
-
-                row++;
+                // Execute the curl command
+                executeCurlCommand(connectionProperty, securityProperty);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static JSONObject createConnectionProperty() {
+    private static JSONObject createConnectionProperty(String[] header, String[] values) {
         JSONObject connectionProperty = new JSONObject();
+        // ... your code to create the connection property JSONObject ...
         connectionProperty.put("displayName", "Connection URL");
         connectionProperty.put("hasAttachment", false);
         connectionProperty.put("hiddenFlag", false);
@@ -65,40 +55,26 @@ executeCurlCommand(connectionProperty, securityProperties);
         connectionProperty.put("propertyName", "connectionUrl");
         connectionProperty.put("propertyShortDesc", "Please make sure that this value really corresponds to the type selected above.");
         connectionProperty.put("propertyType", "URL");
-        connectionProperty.put("propertyValue", "https://testinstance-idevjxz332qf-ia.integration.ocp.oraclecloud.com/");
+        connectionProperty.put("propertyValue", getValueForHeader(header, values, "ConnectionURL"));
         connectionProperty.put("requiredFlag", true);
-    
+
         return connectionProperty;
     }
-    
-    private static JSONObject createSecurityProperty(String propertyName, String propertyValue) {
+
+    private static JSONObject createSecurityProperty(String[] header, String[] values) {
         JSONObject securityProperty = new JSONObject();
-        securityProperty.put("displayName", propertyName);
+        // ... your code to create the security property JSONObject ...
+        securityProperty.put("displayName", "Username");
         securityProperty.put("hasAttachment", false);
         securityProperty.put("hiddenFlag", false);
-        securityProperty.put("propertyDescription", "A " + propertyName.toLowerCase() + " credential");
+        securityProperty.put("propertyDescription", "A username credential");
         securityProperty.put("propertyGroup", "CREDENTIALS");
-        securityProperty.put("propertyName", propertyName.toLowerCase());
+        securityProperty.put("propertyName", "username");
         securityProperty.put("propertyType", "STRING");
-        securityProperty.put("propertyValue", propertyValue);
+        securityProperty.put("propertyValue", getValueForHeader(header, values, "Username"));
         securityProperty.put("requiredFlag", true);
-    
-        return securityProperty;
-    }
-    
-    private static JSONObject createPasswordProperty(String[] header, String[] values) {
-        JSONObject passwordProperty = new JSONObject();
-        passwordProperty.put("displayName", "Password");
-        passwordProperty.put("hasAttachment", false);
-        passwordProperty.put("hiddenFlag", false);
-        passwordProperty.put("propertyDescription", "A password credential");
-        passwordProperty.put("propertyGroup", "CREDENTIALS");
-        passwordProperty.put("propertyName", "password");
-        passwordProperty.put("propertyType", "PASSWORD");
-        passwordProperty.put("propertyValue", getValueForHeader(header, values, "Password"));
-        passwordProperty.put("requiredFlag", true);
 
-        return passwordProperty;
+        return securityProperty;
     }
 
     private static String getValueForHeader(String[] header, String[] values, String headerName) {
@@ -110,22 +86,22 @@ executeCurlCommand(connectionProperty, securityProperties);
         return "";
     }
 
-    private static void executeCurlCommand(JSONObject connectionProperty, List<JSONObject> securityProperties) {
+    private static void executeCurlCommand(JSONObject connectionProperty, JSONObject securityProperty) {
         String authorization = "ZGV2b3BzX3VzZXI6T2ljX0plbmtpbnMjMjAyMw==";
         String endpoint = "https://testinstance-idevjxz332qf-ia.integration.ocp.oraclecloud.com/ic/api/integration/v1/connections/NEWREPOCON";
-    
+
         JSONObject jsonPayload = new JSONObject();
         jsonPayload.put("connectionProperties", connectionProperty);
         jsonPayload.put("securityPolicy", "BASIC_AUTH");
-        jsonPayload.put("securityProperties", securityProperties);
-    
+        jsonPayload.put("securityProperties", securityProperty);
+
         String curlCommand = String.format(
-            "curl --location '%s' --header 'X-HTTP-Method-Override: PATCH' --header 'Content-Type: application/json' --header 'Authorization: Basic %s' --data '%s'",
-            endpoint,
-            authorization,
-            jsonPayload.toString()
+                "curl --header \"Authorization: Basic %s\" --header \"X-HTTP-Method-Override: PATCH\" --header \"Content-Type: application/json\" -d '%s' %s",
+                authorization,
+                jsonPayload.toString(),
+                endpoint
         );
-    
+
         // Execute the curl command
         try {
             ProcessBuilder processBuilder = new ProcessBuilder(curlCommand.split(" "));
@@ -140,5 +116,4 @@ executeCurlCommand(connectionProperty, securityProperties);
             e.printStackTrace();
         }
     }
-    
 }
